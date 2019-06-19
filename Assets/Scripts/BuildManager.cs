a9a463e44f89e3e91c87e83907d4a8b1b6121d37
LittleHerb <LittleHerb@users.noreply.github.com>
2019-06-19 22:13:59 +1000

    Initial commit

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Transform target;
+    public float range = 15f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform wizzFamBoy;
+    public float turnSpeed = 8f;
+
+    public float fireRate = 1f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    private float fireCountdown = 0f;
+
+
+    void Start()
+    {
+        //Repeat UpdateTarget function every X seconds
+        InvokeRepeating("UpdateTarget", 0f, 0.05f);
+    }
+
+    void UpdateTarget()
+    {
+        //Find enemies
+        //find the nearest enemy
+        //set the nearest enemy to the current enemy
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else { target = null; }
+    }
+
+
+    void Update()
+    {
+        //If turret doesnt find any enemies, do nothing!
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(wizzFamBoy.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        wizzFamBoy.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+
+    }
+
+    void Shoot ()
+    {
+        GameObject bulletGO =  (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if (bullet != null)
+        {
+            bullet.Seek(target);
+        }
+    }
+
+    //Draw the range of the turret
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
+
